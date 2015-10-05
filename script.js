4aac1a2573c59416ad867bc398750fb6488f3d33
Sauradeep Paul <sauradeep.paul@gmail.com>
2015-10-05 13:27:47 +0530

    added nav menu

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 document.getElementID("footer01").innerHTML = 
 "<p>&copy;" + new Date().getFullYear() + "(c) Sauradeep Paul</p>";
+
+document.getElementById("navigation01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
